702cb5bf2a9994ae9efd66de1f61867acd1f0bec
Omar Moreno <ormoreno@unipanamericana.edu.co>
2018-02-10 01:01:13 -0500

    design changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  year = 2018;
   constructor() { }
 
   ngOnInit() {
+    const d = new Date();
+    this.year = d.getFullYear();
   }
 
 }
